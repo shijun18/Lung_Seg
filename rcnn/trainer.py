2afd566726bd5b0f5a69495ceba9438687721b36
shijun18 <46990068+shijun18@users.noreply.github.com>
2021-01-27 15:24:07 +0800

    add early stopping

--- rcnn/trainer.py
+++ rcnn/trainer.py
@@ -211,6 +211,8 @@ class SemanticSeg(object):
             lr_scheduler = self._get_lr_scheduler(lr_scheduler, optimizer)
 
         # loss_threshold = 1.0
+        early_stopping = EarlyStopping(patience=20,verbose=True,monitor='val_loss',op_type='min')
+
         for epoch in range(self.start_epoch, self.n_epoch):
             train_loss, train_dice, train_acc = self._train_on_epoch(epoch, net, loss, optimizer, train_loader)
 
@@ -238,6 +240,8 @@ class SemanticSeg(object):
             }, epoch)
             self.writer.add_scalar('data/lr', optimizer.param_groups[0]['lr'],epoch)
 
+            early_stopping(val_loss)
+            #save
             if val_loss <= self.loss_threshold:
                 self.loss_threshold = val_loss
 
@@ -258,9 +262,13 @@ class SemanticSeg(object):
                     val_dice, val_acc)
                 
                 save_path = os.path.join(output_dir, file_name)
-                print("save as %s" % file_name)
+                print("Save as %s" % file_name)
 
                 torch.save(saver, save_path)
+            
+            if early_stopping.early_stop:
+                print('Early Stopping!')
+                break
 
         self.writer.close()
 
@@ -806,3 +814,56 @@ def accuracy(output, target):
     acc = correct.float().sum() / (batch_size*class_num) 
 
     return acc
+
+
+class EarlyStopping(object):
+    """Early stops the training if performance doesn't improve after a given patience."""
+    def __init__(self, patience=10, verbose=True, delta=0, monitor='val_loss',op_type='min'):
+        """
+        Args:
+            patience (int): How long to wait after last time performance improved.
+                            Default: 10
+            verbose (bool): If True, prints a message for each performance improvement. 
+                            Default: True
+            delta (float): Minimum change in the monitored quantity to qualify as an improvement.
+                            Default: 0
+            monitor (str): Monitored variable.
+                            Default: 'val_loss'
+            op_type (str): 'min' or 'max'
+        """
+        self.patience = patience
+        self.verbose = verbose
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.delta = delta
+        self.monitor = monitor
+        self.op_type = op_type
+
+        if self.op_type == 'min':
+            self.val_score_min = np.Inf
+        else:
+            self.val_score_min = 0
+
+    def __call__(self, val_score):
+
+        score = -val_score if self.op_type == 'min' else val_score
+
+        if self.best_score is None:
+            self.best_score = score
+            self.print_and_update(val_score)
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.print_and_update(val_score)
+            self.counter = 0
+
+    def print_and_update(self, val_score):
+        '''print_message when validation score decrease.'''
+        if self.verbose:
+           print(self.monitor, f'optimized ({self.val_score_min:.6f} --> {val_score:.6f}).  Saving model ...')
+        self.val_score_min = val_score
